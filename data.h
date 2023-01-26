fd0103d01ad98e7c2c580a09fb093dba70e2e941
yg17 <yg17@illinois.edu>
2023-01-25 21:09:03 -0600

    Part 04: An Actual Compiler

--- data.h
+++ data.h
@@ -7,5 +7,6 @@
 extern_ int     Line;
 extern_ int     Putback;
 extern_ FILE    *Inputfile;
+extern_ FILE	*Outfile;
 
 extern_ struct token    Token;  // the most recent token scanned in from the input
--- decl.h
+++ decl.h
@@ -8,4 +8,16 @@ struct ASTnode *makeastunary(int op, struct ASTnode *left, int intvalue);
 struct ASTnode *multiplicative_expr(void);
 struct ASTnode *additive_expr(void);
 struct ASTnode *binexpr(void);
+
 int interpretAST(struct ASTnode *n);
+void generatecode(struct ASTnode *n);
+
+void freeall_registers(void);
+void cgpreamble();
+void cgpostamble();
+int cgload(int value);
+int cgadd(int r1, int r2);
+int cgsub(int r1, int r2);
+int cgmul(int r1, int r2);
+int cgdiv(int r1, int r2);
+void cgprintint(int r);
